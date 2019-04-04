59296abf7dccb80668fd3836e479f73ef5b32c6e
Veronica.Morrissette <Veronica.Morrissette@colum.edu>
2019-04-03 20:50:43 -0500

    Ready to Commit VM 190403

---
+++ Demo_SP2019/Demo_SP2019/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_SP2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintToScreen();
+        }
+
+        //make custom method
+        //setting access modifier to public
+        public static void PrintToScreen()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
